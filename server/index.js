ca923086a0214b151a07806da3bfbcbaf62b07c3
ashishraina-rsi <Ashish.Raina@rsystems.com>
2022-01-24 15:43:11 +0530

    converted cod into single page for better understanding the concept: missed first step

--- server/index.js
+++ server/index.js
@@ -1,32 +1,81 @@
-// const express = require("express");
-// const app = express();
-// const PORT = 6969;
-// const cors = require("cors");
-// const graphqlHTTP = require("express-graphql");
 
-// const schema = require('./Schemas/index.js');
-
-// app.use(express.json());
-// app.use(cors());
-
-// app.use('/graphql', graphqlHTTP({
-//     schema, 
-//     graphiql : true
-// }))
-
-// app.listen(PORT, () => {
-//     console.log(`Server Running on ${PORT}!`)
-// });
 
 const express = require("express");
 const app = express();
 const PORT = 6969;
+const graphql = require("graphql");
+const { 
+        GraphQLSchema , 
+        GraphQLObjectType, 
+        GraphQLInt,
+        GraphQLString, 
+        GraphQLList } = graphql;
+
 const { graphqlHTTP } = require("express-graphql");
-const schema = require("./Schemas/index");
 const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
+
+const userData = require("./MOCK_DATA.json");
+
+const UserType = new GraphQLObjectType({
+    name: "User",
+    fields: () => ({
+      id: { type: GraphQLInt },
+      first_name: { type: GraphQLString },
+      last_name: { type: GraphQLString },
+      email: { type: GraphQLString },
+      password: { type: GraphQLString },
+    }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    getAllUsers: {
+      type: new GraphQLList(UserType),
+      args: { id: { type: GraphQLInt } },
+      resolve(parent, args) {
+        return userData;
+      },
+    },
+  },
+});
+
+
+const Mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    createUser: {
+      type: UserType,
+      args: {
+        first_name: { type: GraphQLString },
+        last_name: { type: GraphQLString },
+        email: { type: GraphQLString },
+        password: { type: GraphQLString },
+      },
+      resolve(parent, args) {
+        userData.push({
+          id: userData.length + 1,
+          first_name: args.first_name,
+          last_name: args.last_name,
+          email: args.email,
+          password: args.password,
+        });
+        return args;
+      },
+    },
+  },
+});
+
+const schema = new GraphQLSchema({
+  query :RootQuery,
+  mutation : Mutation
+});
+
+
+
 app.use(
   "/graphql",
   graphqlHTTP({
